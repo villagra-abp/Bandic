d080249393a1caa98d069dca46668b33a486fd9b
Juan Sánchez Marín <juansanchezmarin@192.168.1.104>
2016-10-13 19:26:01 +0200

    mas pruebas

--- Desarrollo/barco.php
+++ Desarrollo/barco.php
@@ -9,13 +9,14 @@
 //ha juanxo le ha petao y estamos aqui probando q flipas
 //otra prueba guapa de juanxo
 echo "hello";
+echo "hello";
 
 
 
 
 
 echo "yi/n";
-
+echo "mis huevos/n";
 echo "mis huevos/n";
 
 ?>
